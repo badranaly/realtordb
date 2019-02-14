a7abfb4022dd4cf5517f21990bbc0b9f10c3ee80
Aly Badran <aly2cool@hotmail.com>
2019-02-14 01:34:40 -0500

    removed console log from login service

--- realtor_client/src/services/loginService.js
+++ realtor_client/src/services/loginService.js
@@ -1,7 +1,6 @@
 import axios  from 'axios';
 
 export const authenticateUser = (username, password) => {
-    console.log("inside authenticate user", username, " ", password);
     return axios('http://localhost:3000/api/', {
         method: "POST",
         data: {
